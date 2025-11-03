c109e02e66cee569a1c99e9662a1151785217c0e
sangwookkhu <ksw090711@gmail.com>
2025-11-03 13:04:20 +0900

    refactor: Lotto class containsmatch, constainsBonus 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,24 +2,50 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoTest {
     @Test
-    void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46, 100})
+    @DisplayName("로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.")
+    void createLottoByOutOfRange(int number) {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, number)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("당첨 번호와 일치하는 개수를 정확히 계산한다.")
+    void countMatch() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = List.of(1, 2, 3, 7, 8, 9);
+
+        assertThat(lotto.countMatch(winningNumbers)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("보너스 번호 포함 여부를 정확히 확인한다.")
+    void containsBonus() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto.containsBonus(6)).isTrue();
+        assertThat(lotto.containsBonus(7)).isFalse();
+    }
 }
